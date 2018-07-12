7afae6090612c47ac73893d2706db0511cac7f3c
DominicLutherEarl <DominicLutherEarl@users.noreply.github.com>
2018-07-12 17:16:59 +0530

    First mobile commit

--- tools/arguments.js
+++ tools/arguments.js
@@ -1,6 +1,8 @@
 // Copyright 2017 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//Writing something to test for editing from mobile. Seems to work. Im in business.
+
 
 class BaseArgumentsProcessor {
   constructor(args) {
